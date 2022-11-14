7a5fc40d36bba03d7ad4ff4c621f81f03d234dc5
1977Suryaa <101641242+1977Suryaa@users.noreply.github.com>
2022-11-14 18:03:10 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
+nose_X=0
+nose_y=0
+left_wrist=0
+right_wrist=0
+var min=0
 function setup(){
     canvas=createCanvas(400,400)
     canvas.position(560,90)
@@ -12,5 +17,22 @@ function modelLoaded(){
 function gotResult(result){
     if(result.length>0){
         console.log(result)
+        nose_X=result[0].pose.nose.x
+        nose_y=result[0].pose.nose.y
+        left_wrist=result[0].pose.leftWrist.x
+        right_wrist=result[0].pose.rightWrist.x
+        min=floor(left_wrist-right_wrist)
+
+        
     }
+}
+function draw(){
+    background("pink")
+    fill("red")
+    stroke("black")
+    textSize(min)
+    text('Hello My Friend',nose_X,nose_y)
+    fill("green")
+    document.getElementById("empty").innerHTML="use your wrist to increase and decrease square size= "+min+"px"
+    
 }
